d361c3b89d48cf70f148de2ad1d0a9c78a08f1f6
Anuj Panchal <anujpan1@gmail.com>
2019-01-25 17:19:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,9 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params[:username], password: params[:password])
     
-    if 
+    if @user
+      
+    end
   end
 
   get '/account' do
