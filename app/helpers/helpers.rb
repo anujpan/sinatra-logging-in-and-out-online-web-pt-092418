540ba5bbc633bc69d5c361d0e0006b7125adcc7e
Anuj Panchal <anujpan1@gmail.com>
2019-01-25 17:19:20 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,15 @@
 class Helpers
   
-  def current_user
-    
+  def self.current_user(session) 
+    User.find_by(id: session[:user_id])
+  end
+
+   def self.is_logged_in?(session)
+    @user = self.current_user(session)
+    if @user
+      true
+    else
+      false
+    end
   end
 end
